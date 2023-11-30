d5701477905f0cd608e2cc5a1700a17e75432d05
Mahenur Islam <mahenurislam97@gmail.com>
2023-11-30 22:43:28 +0600

    user delete api created

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-
 //middleware
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Replace with your frontend URL
+    credentials: true,
+  })
+);
 app.use(express.json());
-
-
-
-console.log(process.env.DB_PASSWORD)
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.neajhbt.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -25,29 +24,70 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    //all collection
+    const userCollection = client.db("wegrow").collection("users");
+    const productCollection = client.db("wegrow").collection("products");
+
+    // users related api
+    app.get("/users", async (req, res) => {
+      const users = await userCollection.find().toArray();
+      const teamMemberCount = users.length; // Get the count of team members
+      res.json({ users, teamMemberCount });
+    });
+
+    //delete an user
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result); // Corrected from req.send to res.send
+    });
+
+
+    
+    //post to userCollection
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      // insert user if email doesn't exist
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exist", insertedId: null });
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
+
+    //creating product collection
+    app.post("/products", async (req, res) => {
+      const item = req.body;
+      const result = await productCollection.insertOne(item);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
-app.get('/', (req, res)=>{
-    res.send('Server is running')
-})
-
-
-app.listen(port, ()=>{
-    console.log(`wegrow server is running on port ${port}`)
-})
+app.listen(port, () => {
+  console.log(`wegrow server is running on port ${port}`);
+});
